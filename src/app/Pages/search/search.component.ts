94da4de68e045402b4d9b3b88765415a01a6717d
Abdulrhman Madi <Abdulrhman.Madi2002@hotmail.com>
2025-03-03 15:12:16 +0200

    add search with api data

--- src/app/Pages/search/search.component.ts
+++ src/app/Pages/search/search.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject ,OnInit} from '@angular/core';
+import { MasterService } from '../../Services/master.service';
+import { Observable } from 'rxjs';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, FormsModule,CommonModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
+masterService = inject(MasterService);
+Locations$ :Observable<any[]> = new Observable<any[]>;
+BusList:any[]=[];
+searchObj:any = {
+  fromLocation: '',
+  tolocation: '',
+  travledata:''
+};
+ngOnInit() {
+this.getAllLocations();
 
 }
+getAllLocations() {
+  this.Locations$ = this.masterService.getLocation();
+}
+onSearch() {
+  if (!this.searchObj.fromLocation || !this.searchObj.tolocation || !this.searchObj.travledata) {
+    alert('Please select valid search options.');
+    return;
+  }
+
+  this.masterService
+    .searchBusLocations(
+      this.searchObj.fromLocation,
+      this.searchObj.tolocation,
+      this.searchObj.travledata
+    )
+    .subscribe({
+      next: (data) => {
+        this.BusList = data;
+      },
+      error: (err) => {
+        console.error('Search failed:', err);
+      }
+    });
+}
+}
+
+
--- src/app/Services/master.service.ts
+++ src/app/Services/master.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MasterService {
+  ApiUrl: string = 'https://localhost:7110/api/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+getLocation(): Observable<any[]> {
+  return this.http.get<any>(this.ApiUrl + 'Location/GetBusLocations').pipe(
+    map(response => response.data)
+  );
+}
+
+
+
+searchBusLocations(
+  from: number,
+  to: number,
+  travelDate: string
+): Observable<any[]> {
+  return this.http
+    .get<any>(`${this.ApiUrl}BusBooking/searchBus?from=${from}&to=${to}&date=${travelDate}`)
+    .pipe(map((response) => response.data));
+}
+
 }
